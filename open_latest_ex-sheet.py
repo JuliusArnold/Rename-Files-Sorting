8c7d8851b1d4c2cdb2e36aed4c7967afb4d315c5
JuliusArnold <julius.arnold@hotmail.com>
2021-11-28 19:18:11 +0100

    Added *.py to open latets exercise-sheets

---
+++ open_latest_ex-sheet.py
@@ -0,0 +1,42 @@
+i#!/usr/bin/env python
+# coding: utf-8
+
+import os 
+import sys
+import numpy as np
+from numpy import sqrt, sin, cos, arcsin
+import random 
+import math as m
+import datetime
+
+import matplotlib.pyplot as plt
+from scipy.optimize import curve_fit
+
+#import pandas
+#import pickle # save Data
+
+# ML Package
+#import tensorflow as tf
+#import cv2 # Picture processing
+#from sciann import Variable, Functional, SciModel, Parameter
+#from sciann.constraints import Data, MinMax
+#from sciann.utils.math import diff
+#import sciann as sn
+#_____________________________________________________________________
+
+
+def get_pwd():
+    pass # Should return current pwd later should run with os.
+
+def main():
+    pwd = get_pwd()
+    #list pdf of pwd 
+    open latest
+    pass
+
+
+
+
+if __name__ == "__main__":
+    main()
+
